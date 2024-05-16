e506c6891ee5384c4738194a93d1ea97055a18cc
Luise <Luise.Muller@gokstadakademiet.no>
2024-05-16 15:29:05 +0200

    added log in and user reg function

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,7 @@
 let crudToken = "Bearer 5sbTeBHD2DNZbeAOwXLeY3aBlXLSC68vnZr5m4asGnq7PxWBNA";
 let baseHref = "https://crudapi.co.uk/api/v1/";
-async function registerUser(username, password) {
+
+export async function registerUser(username, password) {
   let url = `${baseHref}users`;
   let options = {
     method: "POST",
@@ -41,38 +42,44 @@ async function getAllUsers() {
     alert(error);
   }
 }
-async function logInUser(username, password) {
+export async function logInUser(username, password) {
   let allUsers = await getAllUsers();
   let rightUser = allUsers.find((user) => {
     return user.username == username && user.password == password;
   });
-  return rightUser;
+  return rightUser || null;
 }
 
-async function deleteUser(userId){
-    let url = `${baseHref}users/${userId}`;
-    let options = {
-      method: "DELETE",
-      headers: {
-        Authorization: crudToken,
-        "Content-Type": "application/json",
-      }
-    };
-  
-    try {
-      let response = await fetch(url, options);
-      return response.json();
-    } catch (error) {
-      alert(error);
-    }
-};
-export async function checkLoginStatus() {
-   
-    if (isLoggedIn) {
-        document.getElementById('logincontainer').style.display = 'none';
-        document.getElementById('contentContainer').style.display = 'block';
-    } else {
-        document.getElementById('loginContainer').style.display = 'block';
-        document.getElementById('contentContainer').style.display = 'none';
-    }
+async function deleteUser(userId) {
+  let url = `${baseHref}users/${userId}`;
+  let options = {
+    method: "DELETE",
+    headers: {
+      Authorization: crudToken,
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    let response = await fetch(url, options);
+    return response.json();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+export async function checkLoginStatus(user) {
+  let loginContainer = document.getElementById("loginContainer");
+  let contentContainer = document.getElementById("secretContentContainer");
+
+  if (user) {
+    loginContainer.style.display = "none";
+    contentContainer.style.display = "block";
+    document.getElementById(
+      "userGreeting"
+    ).innerText = `Welcome, ${user.username}!`;
+  } else {
+    loginContainer.style.display = "block";
+    contentContainer.style.display = "none";
+  }
 }
--- js/pages/secretlist.js
+++ js/pages/secretlist.js
@@ -1,2 +1,36 @@
-import { checkLoginStatus } from "../backend.js";
+import { checkLoginStatus, logInUser, registerUser } from "../backend.js";
 checkLoginStatus();
+let registerForm = document.getElementById("registerForm");
+let loginForm = document.getElementById("loginForm");
+
+window.logUser = async function (event) {
+  event.preventDefault();
+  let username = event.target.querySelector(
+    'input[placeholder="brukernavn"]'
+  ).value;
+  let password = event.target.querySelector(
+    'input[placeholder="passord"]'
+  ).value;
+  let user = await logInUser(username, password);
+  checkLoginStatus(user);
+};
+
+window.regUser = function (event) {
+  event.preventDefault();
+  let username = event.target.querySelector(
+    'input[placeholder="brukernavn"]'
+  ).value;
+  let password = event.target.querySelector(
+    'input[placeholder="passord"]'
+  ).value;
+  registerUser(username, password);
+};
+
+window.showLoginForm = function () {
+  registerForm.style.display = "none";
+  loginForm.style.display = "block";
+};
+window.showRegisterForm = function () {
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+};
